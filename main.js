7e67680a25d77059a0335c6afc7598eb93c292b6
emborromeo <ericaborromeo06@gmail.com>
2020-07-19 07:54:40 -0700

    new changes

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+let todoLists= [];
+let newContent;
+function addTodo(){
+    let newTodo= document.getElementById("todoText").value;
+    todoLists.push(newTodo);
+    console.log(todoLists);
+    document.getElementById("todoText").value=" ";
+    addtodoList();
+
+}
+
+function addtodoList(){
+    for(i=0;i<todoLists.length;i++){
+         newContent= document.createTextNode(todoLists[i]);  // giving content
+
+    }
+    document.getElementById("taskInLists").innerText=todoLists[0];
+    let newDiv= document.createElement("div");  //creating div element  
+        newDiv.style.height="35px";
+        newDiv.style.color="white";
+        newDiv.style.backgroundColor="#ffd2d2";
+        newDiv.style.width="500px";
+        newDiv.style.fontWeight="bolder";
+        newDiv.style.fontSize="20px";
+        newDiv.style.margin="auto";
+        newDiv.style.border="2px solid #524e4e";
+        newDiv.style.fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+        newDiv.style.marginTop="10px";
+
+    newDiv.classList.add("taskslist");          //setting class name
+   
+
+
+    console.log(newDiv.className);              //trial if the class name is set
+    newDiv.appendChild(newContent);               //adding text node to new div
+    let currentDiv= document.getElementById("lists");   //add new element to dom
+    document.body.insertBefore(newDiv,currentDiv);
+    
+
+} 
