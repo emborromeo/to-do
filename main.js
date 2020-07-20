bbadd75b4e8af7546a92963415dd6500e342642e
emborromeo <ericaborromeo06@gmail.com>
2020-07-20 03:31:56 -0700

    new changes

--- main.js
+++ main.js
@@ -20,55 +20,83 @@ const months = [
 let monthNow=todayDate.getMonth();
 document.getElementById("dateToday").innerHTML=todayDate.toLocaleDateString('en-US', days) + ",  " + months[monthNow]+ " " + dayNow; 
 
+const todoList = document.querySelector(".todo-list");
+
 //Array for todo-list
 let todoLists= [];
 
 //Getting typed todo list value and adding it to array
-let newContent;
-function addTodo(){
-    let newTodo= document.getElementById("todoText").value;
-    todoLists.push(newTodo);
+let newContent; 
+let newTodo;
+function addTodo(e){
+    
+    newTodo=document.getElementById("todoText").value;
+   
+    todoLists.push(newTodo);   
+      for(i=0;i<todoLists.length;i++){
+         newContent= document.createTextNode(todoLists[i]);  // giving content
+    } 
+    createComponents(); 
     document.getElementById("todoText").value=" ";
 
-    addtodoList();
-
-
 }
 
-function addtodoList(){
-    for(i=0;i<todoLists.length;i++){
-         newContent= document.createTextNode(todoLists[i]);  // giving content
-
-    }
 
  //Creating Components (Div)
-    let newDiv= document.createElement("div");  //creating div element  
-        newDiv.style.height="30px";
-        newDiv.style.color="white";
-        newDiv.style.backgroundColor="#9ebec7";
-        newDiv.style.width="300px";
-        newDiv.style.fontWeight="bold";
-        newDiv.style.fontSize="20px";
-        newDiv.style.alignSelf="center";
-        newDiv.style.margin="auto";
-        newDiv.style.borderRadius="8px";
-        newDiv.style.border="none";
-        newDiv.style.boxShadow="2px 2px 2px #898999";
-        newDiv.style.fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
-        newDiv.style.marginTop="10px";
-
-    newDiv.classList.add("tasksList");          //setting class name
-   
-
-
-    console.log(newDiv.className);              //trial if the class name is set
-    newDiv.appendChild(newContent);               //adding text node to new div
-    let currentDiv= document.getElementById("lists");   //add new element to dom
-    document.body.insertBefore(newDiv,currentDiv);
-    
-
-} 
-function createComponents(value)
+function createComponents()
 {
+  
+     console.log(newContent); // viewing array
+
+    const newDiv= document.createElement("div");  //creating div element  
+    newDiv.classList.add("tasksList");  
     
+    const newTodo= document.createElement("li");   //crating li element
+    newTodo.classList.add("todoItem");
+    newDiv.appendChild(newTodo);
+    for(i=0;i<todoLists.length;i++){
+        newTodo.innerText=todoLists[i];
+
+    } 
+    const completeButton= document.createElement("button");  //creating done button
+    completeButton.innerHTML="<i class='check'>/</i>";
+    completeButton.classList.add("checkButton")
+    newDiv.appendChild(completeButton);
+
+    const deleteButton=document.createElement("button");   //creating delete button
+    deleteButton.innerHTML="<i class='delete'>x</i>";
+    deleteButton.classList.add("removeButton");
+    newDiv.appendChild(deleteButton);   
+    todoList.appendChild(newDiv);
+
+
+
 }
+/*
+  const newDiv= document.createElement("div");  //creating div element  
+    newDiv.style.height="30px";
+    newDiv.style.color="white";
+    newDiv.style.backgroundColor="#9ebec7";
+    newDiv.style.width="300px";
+    newDiv.style.fontWeight="bold";
+    newDiv.style.fontSize="20px";
+    newDiv.style.alignSelf="center";
+    newDiv.style.margin="auto";
+    newDiv.style.borderRadius="8px";
+    newDiv.style.border="none";
+    newDiv.style.boxShadow="2px 2px 2px #898999";
+    newDiv.style.fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+    newDiv.style.marginTop="10px";
+
+    newDiv.classList.add("tasksList");  
+    
+    const completeButton= document.createElement("button");  //creating done button
+    completeButton.innerHTML="<i class='check'></i>";
+    completeButton.classList.add("checkButton")
+    newDiv.appendChild(completeButton);
+
+    const deleteButton=document.createElement("button");   //creating delete button
+    deleteButton.innerHTML="<i class='delete'></i>";
+    deleteButton.classList.add("removeButton");
+    newDiv.appendChild(deleteButton);   
+*/
